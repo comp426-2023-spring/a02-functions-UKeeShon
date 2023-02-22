f5348bdb698a10e80c89da88c33cd98fd5fb0013
UKeeShon <ysheng04@email.unc.edu>
2023-02-22 18:12:51 -0500

    add2

--- cli.js
+++ cli.js
@@ -22,4 +22,35 @@ if(args.h){
 	process.exit(0);
 }
 
+let latitude,longtitude;
+if (args.n){
+	latitude = args.n;
+}else if (args.s) {
+	latitude = -args.s;
+}
+if (args.e){
+	longitude = args.e;
+}else if (args.w) {
+	longitude = -args.w;
+}
+
+if (latitude === undefined || Math.abs(latitude)>90){
+	console.error("Latitude mustbe provided and must be within the range of -90 to 90 degrees."
+	process.exit(1);
+}
+if (longtitude === undefined || Math.abs(longitude)>180){
+	console.error("Longtitude must be provided and must be within the range of -180 to 180 degrees.");
+	process,exit(1);
+}
+
+let timezone = moment.tz.guess();
+if(args.t){
+	timezone = args.t;
+}
+
+const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=precipitation_hours&hours&current_weather=true&timezone=${timezone}`;
+const reponse = await fetch(url);
+
+const data = await response.json();
+
 
