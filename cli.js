256bf7a345558d2034c8ada9ba6d2b850361a62b
UKeeShon <ysheng04@email.unc.edu>
2023-02-22 17:42:09 -0500

    add1

--- cli.js
+++ cli.js
@@ -1,9 +1,25 @@
 #!/usr/bin/env node
 
-`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
-	-h	Show this help message and exit.
-	-n, -s	Latitude: N positive; S negative.
-	-e, -w	Longitude: E positive; W negative.
-	-z	Time zone；uses tz.guess() from moment-timezone by default.
-	-d 0-6	Day to retrieve weather: 0 is today; defaults to 1.
-	-j	Echo pretty JSON from open-meteo API and exit.
+import moment from "moment-timezone";
+import minimist from "minimist";
+import fetch from "node-fetch";
+
+const args = minimist(process.argv.slice(2));
+
+if(args.h){
+	console.log(
+
+		`
+		Usage:galosh.js [options] - [n|s] LATITUDE  -[e|w] lONGITUDE -z TIME_ZONE
+		-h 	Show this help message and exit.
+		-n, -s  Latitude: N positive; S negative.
+		-e, -w  Longitude: E positive; W negative.
+		-z	Time zone: uses tz.guess() from moment-timezone by default.
+		-d 0-6  Day to retrieve weather: 0 is today; defaults to 1. 
+		-j	Echo pretty JSON from open -meteo API and exit.
+		`
+	);
+	process.exit(0);
+}
+
+
